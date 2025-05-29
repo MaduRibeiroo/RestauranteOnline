56cacc43eb98e394fa8670ce5c575842e66f17e8
MaduRibeiroo <maadu.ribeiro1404@gmail.com>
2025-05-28 22:08:24 -0300

    restaurante online

--- Trabalho.cpp
+++ Trabalho.cpp
@@ -6,31 +6,198 @@
 
 #include filaPrio.h
 
-//	O programa deve utilizar: Descritor, Listas Din�micas Simplesmente e Duplamente encadeadas, todos ligados, conforme a otimiza��o do processo evidenciado
+//	O programa deve utilizar: Descritor, Listas Dinâmicas Simplesmente e Duplamente encadeadas, todos ligados, conforme a otimiza��o do processo evidenciado
 //do while(!kbhit()&& tempo)
 // insere, retira, volta, esc
 
+void DistribuirPedidos(TpDesc &desc, FILE *arquivo, int qtdPedidos) {
+    int count = 0;
+    TpCozinheiro *cozAtual = desc.inicio;
 
-//descritor e cozinheiro(simplesmente)
-struct TpPont{
-	char cozinheiro[3];
-	TpPont *prox;
-};
+    while (count < qtdPedidos) {
+        if (cozAtual == NULL)
+            cozAtual = desc.inicio;
 
-struct TpDescritor{
-	TpPont *inicio, *fim;
-	int qtde;
-};
+        TpPedido *p = GerarPedidoAleatorio(arquivo);
+        InserirPedidoOrdenado(cozAtual->listaPedidos, p);
+
+        cozAtual = cozAtual->prox;
+        count++;
+    }
+}
+
+int CalcularCarga(TpPedido *lista) {
+    int carga = 0;
+    while (lista != NULL) {
+        carga += lista->tempo;
+        lista = lista->prox;
+    }
+    return carga;
+}
+
+void DistribuirPedidos(TpDesc &desc, FILE *arquivo, int qtdPedidos) {
+    int count = 0;
+
+    while (count < qtdPedidos) {
+        TpPedido *novoPedido = GerarPedidoAleatorio(arquivo);
+
+        TpCozinheiro *escolhido = desc.inicio;
+        int menorCarga = CalcularCarga(desc.inicio->listaPedidos);
+        TpCozinheiro *atual = desc.inicio->prox;
+
+        while (atual != NULL) {
+            int cargaAtual = CalcularCarga(atual->listaPedidos);
+            if (cargaAtual < menorCarga) {
+                menorCarga = cargaAtual;
+                escolhido = atual;
+            }
+            atual = atual->prox;
+        }
+
+        InserirPedidoOrdenado(escolhido->listaPedidos, novoPedido);
+
+        // Atualiza estado do cozinheiro escolhido
+        if (escolhido->listaPedidos != NULL)
+            escolhido->estado = 'O';
+
+        count++;
+    }
+
+    // Após a distribuição, atualizar o estado dos demais
+    TpCozinheiro *temp = desc.inicio;
+    while (temp != NULL) {
+        if (temp->listaPedidos == NULL)
+            temp->estado = 'L';
+        temp = temp->prox;
+    }
+}
 
 
 
-TpPont *NovaCaixaPedido(char tipo[15], itens[50][50], int tempo, int prioridade){
+//caixa para cuxinehiro
+TpPont *NovaCaixaCozinheiro(int id, int estado){
 	TpPedido *caixa = new TpPedido;
+	caixa -> id = id;
+	caixa -> estado = L; //L de Lula
 	caixa -> prox = NULL;
-	caixa ->ant = NULL;
-	strcpy(caixa -> tipo, tipo);
-	caixa -> tempo = tempo;
-	caixa -> prioridade = prioridade;
+	caixa -> listaPedidos = NULL;
 	return caixa;
 }
 
+
+TpPedido* GerarPedidoAleatorio() {
+    FILE *ptr;
+    TpPedido *pedido = NULL;
+    int tempo = 0, linha_aleatoria = 0, linha_atual = 1;
+    char buffer[256], tipo_str[20], itens[256];
+
+    ptr = fopen("Pedidos.txt", "r");
+    if (ptr != NULL) {
+        linha_aleatoria = 1 + (rand() % 15); // Supondo até 15 linhas
+        rewind(ptr);
+
+        while (linha_atual <= linha_aleatoria) {
+            if (fgets(buffer, sizeof(buffer), ptr) != NULL) {
+                if (linha_atual == linha_aleatoria) {
+                    pedido = new TpPedido;
+
+                    sscanf(buffer, "%[^,],%d,\"%[^\"]\"", tipo_str, &tempo, itens);
+
+                    strcpy(pedido->tipo, tipo_str);
+                    strcpy(pedido->itens, itens);
+                    pedido->tempo = tempo;
+
+                    if (strcmp(tipo_str, "Expresso") == 0)
+                        pedido->prioridade = 1;
+                    else if (strcmp(tipo_str, "Regular") == 0)
+                        pedido->prioridade = 2;
+                    else
+                        pedido->prioridade = 3;
+
+                    pedido->prox = NULL;
+                    pedido->ant = NULL;
+                }
+                linha_atual++;
+            } else {
+                // Força saída do laço caso erro de leitura (sem usar break)
+                linha_atual = linha_aleatoria + 1;
+            }
+        }
+
+        fclose(ptr);
+    }
+
+    return pedido; // permitido aqui, pois está no fim da função
+}
+
+
+void RemoverCozinheiro(TpCozinheiro* &lista, int id) {
+    TpCozinheiro *anterior = NULL;
+    TpCozinheiro *atual = lista;
+
+    while (atual != NULL) {
+        if (atual->id == id) {
+            if (anterior == NULL) {
+                lista = atual->prox;
+            } else {
+                anterior->prox = atual->prox;
+            }
+            TpPedido *p = atual->listaPedidos;
+            TpPedido *aux;
+            while (p != NULL) {
+                aux = p->prox;
+                delete p;
+                p = aux;
+            }
+
+            delete atual;
+            atual = NULL; 
+        } else {
+            anterior = atual;
+            atual = atual->prox;
+        }
+    }
+}
+
+void Menu() {
+    int op;
+
+    do {
+        printf("\n====== MENU ======\n");
+        printf("1. Escolher quantidade de cozinheiros\n");
+        printf("2. Remover um cozinheiro\n");
+        printf("3. Simular\n");
+        printf("4. Listar cozinheiros\n");
+        printf("ESC. Sair\n");
+        printf("Escolha: ");
+        scanf("%d", &op);
+
+        switch (op) {
+            case 1: {
+                int qtd, i= 1;
+                printf("Quantos cozinheiros deseja adicionar? ");
+                scanf("%d", &qtd);
+				while(i<=qtd){
+					NovaCaixaCozinheiro(i);
+					i++;
+				}
+                break;
+            }
+            case 2: {
+                int id;
+                printf("ID do cozinheiro a remover: ");
+                scanf("%d", &id);
+                RemoverCozinheiro(&listaCozinheiros, id);
+                break;
+            }
+            case 3:
+                Simular();
+                break;
+            case 4:
+                ListarCozinheiros(listaCozinheiros);
+                break;
+            default:
+                printf("Opção inválida!\n");
+        }
+    } while (op != 27);
+}
--- tad.cpp
+++ tad.cpp
@@ -1,42 +1,109 @@
-#define MAXFILA 200
+//descritor e cozinheiro(simplesmente)
+struct TpDesc{	
+	TpPont *inicio, *fim;
+	int tamanho;
+};
+
+struct TpCozinheiro{
+	int id;
+	char estado;
+	TpPedido *listaPedidos;
+	TpCozinheiro *prox;
+};
 
 //duplamente encadeada pra pedido
 struct TpPedido{
-	char tipo[15], itens[50][50];
+	char tipo[15], itens[100];
 	int tempo, prioridade; //1 expresso(alta), 2 regular(media), 3 pre agendado(baixa)
 	TpPedido *ant, *prox;
 };
 
-struct TpFila{
-	int fim;
-	TpPedido fila[MAXFILA];
+struct TpFila {
+    TpPedido *inicio, *fim;
 };
 
-void inicializar(TpFila &f){
-	f.fim=-1;
-}
+TpPedido *NovoPedido(char tipo[15], char itens[100], int tempo) {
+    TpPedido *p = new TpPedido;
+    strcpy(p->tipo, tipo);
+    strcpy(p->itens, itens);
+    p->tempo = tempo;
 
-int vazia(int fim){
-	return fim == -1;
-}
-
-int cheia(int fim){
-	return fim == MAXFILA-1;
-}
-
-void inserir(TpFila &f, TpPedido p){
-	TpPedido aux;
-	//verifica prioridade
-	if(stricmp(p->tipo, "Regular") == 0)
+    if (stricmp(tipo, "Expresso") == 0) 
 		p->prioridade = 1;
-	else
-		if(stricmp(p->tipo, "Expresso") == 0)
+    else{
+		if (stricmp(tipo, "Regular") == 0) 
 			p->prioridade = 2;
-		else
-			if(stricmp(p->tipo, "Pré-Agendado") == 0)
-				p->prioridade = 3;
-	//insere o pedido na fila
-	
+    	else 
+			p->prioridade = 3;
+	}
+    p->ant = p->prox = NULL;
+
+    return p;
 }
+
+void InserirPedidoOrdenado(TpPedido* &lista, TpPedido* novo) {
+    TpPedido *atual = lista;
+
+    if (lista == NULL) {
+        lista = novo;
+    } else {
+        while (atual != NULL && atual->prioridade <= novo->prioridade) {
+            atual = atual->prox;
+        }
+
+        if (atual == lista) {
+            novo->prox = lista;
+            lista->ant = novo;
+            lista = novo;
+        }
+		else 
+			if (atual == NULL) {
+				TpPedido *fim = lista;
+				while (fim->prox != NULL) fim = fim->prox;
+				fim->prox = novo;
+				novo->ant = fim;
+			} 
+			else {
+				novo->prox = atual;
+				novo->ant = atual->ant;
+				atual->ant->prox = novo;
+				atual->ant = novo;
+        	}
+    }
+}
+
+void InicializarCozinheiros(TpDesc &desc, int qtd) {
+    desc.inicio = desc.fim = NULL;
+    desc.tamanho = 0;
+
+    for (int i = 1; i <= qtd; i++) {
+        TpCozinheiro *novo = new TpCozinheiro;
+        novo->id = i;
+        novo->estado = true;
+        novo->prox = NULL;
+        novo->listaPedidos = NULL;
+
+        if (desc.inicio == NULL) {
+            desc.inicio = desc.fim = novo;
+        } else {
+            desc.fim->prox = novo;
+            desc.fim = novo;
+        }
+
+        desc.tamanho++;
+    }
+}
+
+
+void ImprimirFila(TpFila f) {
+    TpPedido* p = f.inicio;
+    while (p != NULL) {
+        printf("Tipo: %s | Itens: %s | Tempo: %d | Prioridade: %d\n", 
+            p->tipo, p->itens, p->tempo, p->prioridade);
+        p = p->prox;
+    }
+}
+
+
 
 
